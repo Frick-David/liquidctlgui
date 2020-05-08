f7722941b1a3e69e02ba7e6748baa23587a18887
Frick-David <frick-david@protonmail.com>
2020-05-08 02:25:53 -0400

    WIP - Started System Tray. Added it to the app. Will need to add system tray options next

--- run_new.py
+++ run_new.py
@@ -1,10 +1,14 @@
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QTabWidget, QVBoxLayout, QPushButton,
-                             QMainWindow, QLabel, QToolBar, QAction, QStatusBar)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QTabWidget,
+                             QVBoxLayout, QPushButton, QMainWindow, QLabel,
+                             QToolBar, QAction, QStatusBar, QSystemTrayIcon,
+                             QMenu)
 from PyQt5.QtGui import QIcon
-import sys
 from PyQt5 import QtCore
+import sys
+
 
 class App(QMainWindow):
+
     def __init__(self, *args, **kwargs):
         super(App, self).__init__(*args, **kwargs)
         self.setWindowTitle("My Awesome App")
@@ -13,6 +17,7 @@ class App(QMainWindow):
         self.top = 10
         self.width = 640
         self.height = 480
+        self.has_systray = QSystemTrayIcon.isSystemTrayAvailable()
         label = QLabel("Liquidctl")
         label.setAlignment(QtCore.Qt.AlignCenter)
         self.setCentralWidget(label)
@@ -35,15 +40,27 @@ class App(QMainWindow):
         self.setStatusBar(QStatusBar(self))
         self.initUI()
 
+        # Add setting for minimize to Tray
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
 
+    def set_sys_tray_icon(self):
+        if self.has_systray:
+            icon = QSystemTrayIcon(QIcon("icon.jpg"), self)
+            icon_menu = QMenu('Test')
+            icon_menu.addAction('Foo')
+            icon_menu.addSeparator()
+            icon_menu.addAction('Bar')
+            icon.setContextMenu(icon_menu)
+            icon.show()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.set_sys_tray_icon()
         self.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
